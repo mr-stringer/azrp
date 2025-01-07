18769eb834a4facb9ab8404ab42e73b76deeb8e5
Steven Stringer <sstringer@gentlemanfox.co.uk>
2025-01-07 13:15:26 +0000

    added execall

--- api.go
+++ api.go
@@ -2,6 +2,7 @@ package azrp
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -18,6 +19,51 @@ func apiGet(url string) (ApiResponse, error) {
 	err = jdec.Decode(&ar)
 	if err != nil {
 		return ApiResponse{}, err
+	}
+	return ar, nil
+}
+
+// apiGetAll is another version of the apiGetter function. This version checks
+// the version returns all results by checking the NextResultLink field and
+// keeps fetching items until the list is exhausted.
+func apiGetAll(url string) (ApiResponse, error) {
+	fmt.Println("Getting", url)
+	r1, err := http.Get(url)
+	if err != nil {
+		return ApiResponse{}, err
+	}
+	defer r1.Body.Close()
+	ar := ApiResponse{}
+	jdec := json.NewDecoder(r1.Body)
+	err = jdec.Decode(&ar)
+	if err != nil {
+		return ApiResponse{}, err
+	}
+
+	next := ar.NextPageLink
+
+	for {
+		if next == "" {
+			break
+		} else {
+			fmt.Println("Getting", next)
+			r2, err := http.Get(next)
+			if err != nil {
+				return ApiResponse{}, err
+			}
+			defer r2.Body.Close()
+			arTmp := ApiResponse{}
+			jdec := json.NewDecoder(r2.Body)
+			err = jdec.Decode(&arTmp)
+			if err != nil {
+				return ApiResponse{}, nil
+			}
+			//Now copy the items to the original response
+			ar.Items = append(ar.Items, arTmp.Items...)
+			//Update next link
+			next = arTmp.NextPageLink
+
+		}
 	}
 	return ar, nil
 }
